494635d8aa65539dd249c1781e894e01790fd25f
Derek Lindahl <dlindahl@customink.com>
2013-04-29 14:07:26 -0400

    Adds RSpec

---
+++ spec/frenetic_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe Frenetic do
+  it 'is pending'
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,9 @@
+ENV['RAILS_ENV'] ||= 'test'
+
+require 'awesome_print'
+require 'rspec'
+require 'frenetic'
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir['./spec/support/**/*.rb'].each {|f| require f}
