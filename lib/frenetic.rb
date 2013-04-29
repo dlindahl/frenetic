49120996cde53ede21a41a87a67b9ae31275fa83
Derek Lindahl <dlindahl@customink.com>
2013-04-29 14:37:06 -0400

    Adds some initial delegation tests

--- lib/frenetic.rb
+++ lib/frenetic.rb
@@ -1,4 +1,14 @@
+require 'faraday'
+
 require 'frenetic/version'
 
 class Frenetic
+  extend Forwardable
+  def_delegators :connection, :get, :put, :post, :delete
+
+  Error = Class.new(StandardError)
+
+  def connection
+    @connection ||= Faraday.new
+  end
 end
--- spec/frenetic_spec.rb
+++ spec/frenetic_spec.rb
@@ -1,5 +1,51 @@
 require 'spec_helper'
 
 describe Frenetic do
-  it 'is pending'
+  subject(:instance) { described_class.new }
+
+  describe '#connection' do
+    subject { instance.connection }
+
+    it { should be_a Faraday::Connection }
+  end
+
+  describe '#get' do
+    subject { instance.get '/foo' }
+
+    it 'should delegate to Faraday' do
+      instance.connection.should_receive :get
+
+      subject
+    end
+  end
+
+  describe '#put' do
+    subject { instance.put '/foo' }
+
+    it 'should delegate to Faraday' do
+      instance.connection.should_receive :put
+
+      subject
+    end
+  end
+
+  describe '#post' do
+    subject { instance.post '/foo' }
+
+    it 'should delegate to Faraday' do
+      instance.connection.should_receive :post
+
+      subject
+    end
+  end
+
+  describe '#delete' do
+    subject { instance.delete '/foo' }
+
+    it 'should delegate to Faraday' do
+      instance.connection.should_receive :delete
+
+      subject
+    end
+  end
 end
